c25fa080b312e9c810e001e5ad36552866e363c7
Dimon <d.chistov11@gmail.com>
2023-01-21 19:53:33 +0600

    change val

--- src/main/kotlin/lesson_4/task_1.kt
+++ src/main/kotlin/lesson_4/task_1.kt
@@ -1,8 +1,8 @@
 package lesson_4
 
 fun main() {
-    val bookedTableToday = 1
-    val bookedTableTomorrow = 13
+    val bookedTableToday = 13
+    val bookedTableTomorrow = 4
 
     println("[Доступность столиков на сегодня: ${bookedTableToday < TOTAL_TABLE}]\n[Доступность столиков на завтра: ${bookedTableTomorrow < TOTAL_TABLE}]")
 
